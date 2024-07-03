f99d3cbb4486657f2903bd70253cc377dfb7be7d
HerrPhil <phillip.j.wray.esq@gmail.com>
2024-07-02 21:31:19 -0600

    Create a Position class to allow easy use of collections of positions

---
+++ Position.java
@@ -0,0 +1,23 @@
+public final class Position {
+
+    private int i, j;
+
+    public Position(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    @Override
+    public boolean equals(Object position) {
+        if (position == null) return false;
+        if (this == position) return true;
+        if (!(position instanceof Position)) return false;
+        Position that = (Position) position;
+        return this.i == that.i && this.j == that.j;
+    }
+
+    @Override
+    public String toString() {
+        return "Position (" + String.valueOf(i) + ", " + String.valueOf(j) + ")";
+    }
+}
