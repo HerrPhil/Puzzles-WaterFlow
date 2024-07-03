e3e29aeae7daa89749b9932e23fe950fe0cb173c
HerrPhil <phillip.j.wray.esq@gmail.com>
2024-07-02 21:32:00 -0600

    Initialize the shoreline positions in the queues and sets of seen positions

--- WaterFlow.java
+++ WaterFlow.java
@@ -12,7 +12,7 @@ public class WaterFlow {
 
         WaterFlow waterFlow = new WaterFlow();
 
-        List<List<Integer>> positions = new ArrayList<>();
+        List<List<Integer>> elevations = new ArrayList<>();
 
         int [] column1 = new int [] {1, 3, 2, 6, 5};
         List<Integer> list1 = Arrays.stream(column1).boxed().collect(Collectors.toList());
@@ -29,23 +29,78 @@ public class WaterFlow {
         int [] column5 = new int [] {5, 4, 1, 5, 4};
         List<Integer> list5 = Arrays.stream(column5).boxed().collect(Collectors.toList());
 
-        positions.add(list1);
+        elevations.add(list1);
 
-        positions.add(list2);
+        elevations.add(list2);
 
-        positions.add(list3);
+        elevations.add(list3);
 
-        positions.add(list4);
+        elevations.add(list4);
 
-        positions.add(list5);
+        elevations.add(list5);
         
-        Set<int[]> result = waterFlow.findPositionsToFlowIntoBothOceans(positions);
+        Set<Position> result = waterFlow.findPositionsToFlowIntoBothOceans(elevations);
 
         System.out.printf("number of positions = %d%n", result.size());
     }
 
-    public Set<int[]> findPositionsToFlowIntoBothOceans(List<List<Integer>> positions) {
-        Set<int[]> result = new HashSet<>();
+    public Set<Position> findPositionsToFlowIntoBothOceans(List<List<Integer>> elevations) {
+
+        // Here we leverage that fact that java.util.LinkedList
+        // implements java.util.Deque, which gives us the
+        // queuing behaviour we want for BFS.
+
+        Deque<Position> pacificQueue = new LinkedList<>();
+        Set<Position> pacificSeen = new HashSet<>();
+
+        Deque<Position> atlanticQueue = new LinkedList<>();
+        Set<Position> atlanticSeen = new HashSet<>();
+
+        int m = elevations.size();
+
+        int n = elevations.get(0).size();
+
+        // We assume the pacific shore is from the top-left corner
+        // to the top-right corner and bottom-left corner.
+
+        // Add all the pacific shoreline positions that flow to the pacific
+        // to the the pacific queue and pacific seen set.
+        for (int j = 0; j < n; j++) {
+            Position position = new Position(0, j);
+            pacificQueue.addLast(position);
+            pacificSeen.add(position);
+        }
+
+        // Start at 1 to avoid duplicating the position at 0 added above.
+        for (int i = 1; i < m; i++) {
+            Position position = new Position(i, 0);
+            pacificQueue.addLast(position);
+            pacificSeen.add(position);
+        }
+
+        // Add all the atlantic shorelinee positions that flow to the atlantic
+        // to the atlantic queue and atlantic set.
+        for (int i = 0; i < m; i++) {
+            Position position = new Position(i, n - 1);
+            atlanticQueue.addLast(position);
+            atlanticSeen.add(position);
+        }
+
+        // Stop before n - 1 to avoid duplicating the position at n - 1 added above.
+        for (int j = 0; j < n - 1; j++) {
+            Position position = new Position(m - 1, j);
+            atlanticQueue.addLast(position);
+            atlanticSeen.add(position);
+        }
+
+        System.out.printf("Expect 9 queue pacific shoreline positions, actual queue = %d%n", pacificQueue.size());
+        System.out.printf("Expect 9 seen pacific shoreline positions, actual seen = %d%n", pacificSeen.size());
+                
+        System.out.printf("Expect 9 queue atlantic shoreline positions, actual queue = %d%n", atlanticQueue.size());
+        System.out.printf("Expect 9 seen atlantic shoreline positions, actual seen = %d%n", atlanticSeen.size());
+
+        // This will be the intersection of pacific and atlantic positions.
+        Set<Position> result = new HashSet<>();
 
         return result;
     }
